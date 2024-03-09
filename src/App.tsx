d07b4dee2fe552aade53e8623a518df6e635464a
Rost Kaniuchenko <rkaniuchenko@gmail.com>
2024-03-09 15:30:24 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useCallback } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const handleClick = useCallback(() => {
+    const getTabs = async () => {
+      const tabs = await chrome.tabs.query({});
+      const groups = await chrome.tabGroups.query({});
+      console.log(tabs, groups);
+    };
+    void getTabs();
+  }, []);
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleClick}>Tabs</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
