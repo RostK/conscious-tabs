7abbbd33c3a2e6788a2851c1a158eace68763742
Rost Kaniuchenko <rkaniuchenko@gmail.com>
2024-03-25 18:06:55 +0000

    Drop group on group

--- src/lib/Tabs/TabsGroup/handleDrop.ts
+++ src/lib/Tabs/TabsGroup/handleDrop.ts
@@ -14,6 +14,24 @@ export const handleDrop: DZonDrop<GroupItem> = async (drop, dropzone) => {
       await chrome.tabs.ungroup(drop.id as number);
       return;
     case "group":
+      if (dropzone.data.windowId === drop.windowId) {
+        try {
+          await chrome.tabGroups.move(drop.id, {
+            //If dragged tab is before dropped, index has to be changed
+            index:
+              dropzone.data.tabs[0].index > drop.tabs[0].index
+                ? dropzone.data.tabs[0].index - drop.tabs.length
+                : dropzone.data.tabs[0].index,
+          });
+        } catch (e) {
+          console.error(e);
+        }
+      } else {
+        await chrome.tabGroups.move(drop.id, {
+          index: dropzone.data.tabs[0].index,
+          windowId: dropzone.data.windowId,
+        });
+      }
       return;
   }
 };
